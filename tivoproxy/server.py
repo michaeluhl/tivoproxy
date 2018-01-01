0df744e0376813710cac7a4837e312cc626f719e
Michael Uhl <michael.uhl@gmail.com>
2017-12-31 22:52:53 -0500

    Add basic protocol support.

--- tivoproxy/server.py
+++ tivoproxy/server.py
@@ -1,4 +1,3 @@
-import argparse
 import configparser
 import io
 import threading
@@ -10,17 +9,52 @@ from pubnub.pubnub import PubNub
 
 
 class ServedObject(object):
+    # Request format:
+    # {
+    #   type='request',
+    #   cmd='cmd',
+    #   params={
+    #     param_a=value_a,
+    #     param_b=value_b
+    #   }
+    # }
+    #
+    # Response:
+    # {
+    #   type='response',
+    #   cmd='cmd',
+    #   result={}
+    # }
+    # If there's an error, there may be an 'error' field in the response
 
     def __init__(self, server):
         self.server = server
 
     def handle_message(self, pubnub, message):
-        raise NotImplementedError()
+        content = message.message
+        try:
+            cmd = content['cmd']
+        except KeyError as e:
+            return {'type': 'response',
+                    'error': 'Message does not contain a command directive.'}
+        try:
+            cmd_fn = getattr(self, 'do_' + cmd)
+        except AttributeError as e:
+            return {'type': 'response',
+                    'cmd': cmd,
+                    'error': 'Command ({}) is not a valid directive.'.format(cmd)}
+        try:
+            return cmd_fn(**content['params'])
+        except TypeError as e:
+            return {'type': 'response',
+                    'cmd': cmd,
+                    'error': 'Incorrect number of parameters for command.'}
+
 
 
 class PNObjectServer(SubscribeCallback):
 
-    def __init__(self, config, served_class, server_name="PNOjbectServer"):
+    def __init__(self, config, served_class, server_name="PNObjectServer"):
         self.__config = config
         self.__server_name = server_name
         self.__served_class = served_class
@@ -33,6 +67,7 @@ class PNObjectServer(SubscribeCallback):
         self.__pnconfig.publish_key = pn_cfg['PUBKEY']
         self.__pnconfig.subscribe_key = pn_cfg['SUBKEY']
         self.__pnconfig.uuid = pn_cfg['CLIENT_ID']
+        self.__pnconfig.ssl = True
 
         self.__channel_pub = pn_cfg['PUBLISH_CHANNEL']
         self.__channel_sub = pn_cfg['SUBSCRIBE_CHANNEL']
@@ -70,7 +105,8 @@ class PNObjectServer(SubscribeCallback):
 
     def message(self, pubnub, message):
         if self.__served_object is not None:
-            self.__served_object.handle_message(pubnub=pubnub, message=message)
+            response = self.__served_object.handle_message(pubnub=pubnub, message=message)
+            pubnub.publish().channel(self.__channel_pub).message(response).sync()
 
     def run(self):
         self.__served_object = self.__served_class(self)
